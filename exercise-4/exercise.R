844d84ae96a5c8b70505484602ab12832d8008de
Student User <studentuser@D-173-250-249-205.dhcp4.washington.edu>
2018-04-16 16:46:28 -0700

    finish exercise 4

--- exercise-4/exercise.R
+++ exercise-4/exercise.R
@@ -9,36 +9,48 @@
 # What are the names of the columns?
 # Use `??flights` to search for documentation on the data set (for what the 
 # columns represent)
-
+nrow(flights)
+ncol(flights)
+colnames(flights)
+?flights
 
 # Use `dplyr` to give the data frame a new column that is the amount of time
 # gained or lost while flying (that is: how much of the delay arriving occured
 # during flight, as opposed to before departing).
-
+flights <- mutate(flights, time_gained = arr_delay - dep_delay)
 
 # Use `dplyr` to sort your data frame in descending order by the column you just
 # created. Remember to save this as a variable (or in the same one!)
-
+flights <- arrange(flights, desc(time_gained))
 
 # For practice, repeat the last 2 steps in a single statement using the pipe
 # operator. You can clear your environmental variables to "reset" the data frame
-
+flights <- flights %>%> mutate(time_gained = arr_delay - dep_delay) %>%> arrange(desc(time_gained))
 
 # Make a histogram of the amount of time gained using the `hist()` function
-
+hist(flights$time_gained)
 
 # On average, did flights gain or lose time?
 # Note: use the `na.rm = TRUE` argument to remove NA values from your aggregation
-
+mean(flights$time_gained, na.rm = TRUE)
 
 # Create a data.frame of flights headed to SeaTac ('SEA'), only including the
 # origin, destination, and the "gain_in_air" column you just created
-
+depart_to_SeaTac <- flights %>%> 
+  filter(dest == "SEA") %>%>
+  select(origin, dest, time_gained)
 
 # On average, did flights to SeaTac gain or loose time?
-
+mean(depart_to_SeaTac, na.rm = TRUE)
 
 # Consider flights from JFK to SEA. What was the average, min, and max air time
 # of those flights? Bonus: use pipes to answer this question in one statement
 # (without showing any other data)!
+summary <- flights %>%>
+    filter(origin == "JFK", dest == "SEA") %>%>
+  summarize(
+    avg_time = mean(time_gained, na.rm = TRUE),
+    min_time = min(time_gained, na.rm = TRUE),
+    max_time = max(time_gained, na.rm = TRUE),
+  )
 
